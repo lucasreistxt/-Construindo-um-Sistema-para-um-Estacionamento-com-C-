4fc1ba495f1a751ea92229091e9018fec870d15b
Lucas Reis <100703776+lucasreistxt@users.noreply.github.com>
2023-12-18 13:28:23 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,92 +1,59 @@
 using DesafioFundamentos.Models;
-using System;
-using System.Diagnostics;
 
-[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-class Program
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
 {
-    static void Main()
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        case "1":
+            es.AdicionarVeiculo();
+            break;
 
-        decimal precoInicial = ObterDecimalInput("Seja bem vindo ao sistema de estacionamento!\nDigite o preço inicial:");
-        decimal precoPorHora = ObterDecimalInput("Agora digite o preço por hora:");
+        case "2":
+            es.RemoverVeiculo();
+            break;
 
-        Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+        case "3":
+            es.ListarVeiculos();
+            break;
 
-        string opcao = string.Empty;
-        bool exibirMenu = true;
+        case "4":
+            exibirMenu = false;
+            break;
 
-        while (exibirMenu)
-        {
-            Console.Clear();
-            Console.WriteLine("Digite a sua opção:");
-            Console.WriteLine("1 - Cadastrar veículo");
-            Console.WriteLine("2 - Remover veículo");
-            Console.WriteLine("3 - Listar veículos");
-            Console.WriteLine("4 - Encerrar");
-
-            opcao = Console.ReadLine();
-
-            switch (opcao)
-            {
-                case "1":
-                    es.AdicionarVeiculo();
-                    Console.WriteLine("Veículo cadastrado com sucesso!");
-                    break;
-
-                case "2":
-                    es.RemoverVeiculo();
-                    break;
-
-                case "3":
-                    es.ListarVeiculos();
-                    break;
-
-                case "4":
-                    exibirMenu = false;
-                    break;
-
-                default:
-                    Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
-                    break;
-            }
-
-            if (exibirMenu)
-            {
-                Console.WriteLine("Pressione uma tecla para continuar");
-                Console.ReadLine();
-            }
-        }
-
-        Console.WriteLine("O programa se encerrou. Pressione uma tecla para sair.");
-        Console.ReadLine();
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
 
-    static decimal ObterDecimalInput(string mensagem)
-    {
-        decimal valor = 0;
-        bool entradaValida = false;
-
-        while (!entradaValida)
-        {
-            Console.WriteLine(mensagem);
-
-            if (decimal.TryParse(Console.ReadLine(), out valor))
-            {
-                entradaValida = true;
-            }
-            else
-            {
-                Console.WriteLine("Por favor, insira um valor numérico válido.");
-            }
-        }
-
-        return valor;
-    }
-
-    private string GetDebuggerDisplay()
-    {
-        return ToString();
-    }
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
 }
+
+Console.WriteLine("O programa se encerrou");
